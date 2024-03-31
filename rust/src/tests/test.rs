2075a902d687921e2c184cb45af8c93f726b2833
vasco <vasco.schiavo@protonmail.com>
2024-03-31 23:41:31 +0200

    add is_prime method with tests

--- rust/src/tests/test.rs
+++ rust/src/tests/test.rs
@@ -3,6 +3,16 @@ pub mod tests {
     use rstest::rstest;
     use crate::utils::functions::functions::number_of_divisors;
     use crate::utils::functions::functions::triangular_number;
+    use crate::utils::functions::functions::is_prime;
+
+    #[rstest]
+    #[case(2, true)]
+    #[case(3, true)]
+    #[case(7, true)]
+    #[case(10, false)]
+    fn test_is_prime(#[case] input: u32, #[case] expected: bool) {
+        assert_eq!(expected, is_prime(input));
+    }
 
     #[rstest]
     #[case(1, 1)]
--- rust/src/utils/functions.rs
+++ rust/src/utils/functions.rs
@@ -1,5 +1,20 @@
 pub mod functions {
 
+    pub fn is_prime(n: u32) -> bool {
+        if n <= 3 {
+            return true;
+        };
+        if n % 2 == 0 || n % 3 == 0 {
+            return false;
+        };
+        for divisor in (5..=((n as f64).sqrt() as u32) + 1).step_by(2){
+            if n % divisor == 0 || n % (divisor + 2) == 0 {
+                return false;
+            }
+        };
+        return true;
+    }
+
     pub fn number_of_divisors(n: &u32) -> u32 {
         let mut number_of_divisors: u32 = 0;
 
